fe3ddce138c5613c5074bda4e961e666819006ea
Lina Chen <sakura0477@gmail.com>
2020-01-02 22:33:30 +0800

    Add Mongoose connection and create Urlshorten model

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+mongoose.connect('mongodb://localhost/urlshorten', { useUnifiedTopology: true, useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const Urlshorten = require('./models/urlshorten')
 
 app.get('/', (req, res) => {
   res.render('index')
---
+++ models/urlshorten.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const shortenSchema = new Schema({
+  url_origin: {
+    type: String,
+    required: true
+  },
+  url_shorten: {
+    type: String
+  }
+})
+
+module.exports = mongoose.model('Todo', shortenSchema)
