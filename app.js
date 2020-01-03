d31786d43428e8a6257509488f03642b2cd62ad4
Lina Chen <sakura0477@gmail.com>
2020-01-03 22:01:02 +0800

    Add warning message to avoid no input

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const urlShorten = require('./urlshorten')
+const Urlshorten = require('./models/urlshorten')
 const port = 3000
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
@@ -22,26 +23,28 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-const Urlshorten = require('./models/urlshorten')
-
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  const shortener = urlShorten(req.body)
-
-
-  const url_shorten = new Urlshorten({
-    url_origin: req.body.urlshorten,
-    url_shorten: shortener
-  })
-  // 存入資料庫
-  url_shorten.save(err => {
-    if (err) return console.error(err)
-    res.render('index', { shortener: shortener })
-  })
-
+  //判斷輸入的url是否為空
+  if (req.body.urlshorten) {
+    const shortener = urlShorten(req.body)
+    const url_shorten = new Urlshorten({
+      url_origin: req.body.urlshorten,
+      url_shorten: shortener
+    })
+    // 存入資料庫
+    url_shorten.save(err => {
+      if (err) return console.error(err)
+      res.render('index', { shortener: shortener })
+    })
+  } else {
+    //若為空則提醒使用者須先輸入url
+    const warning = '請先輸入要縮短的URL網址'
+    res.render('index', { warning: warning })
+  }
 
 })
 
